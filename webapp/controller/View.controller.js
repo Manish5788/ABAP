e0f298df6909079be6e6ba4b4fdcf970546a97a7
MANISH RAJAK <manish.kumar.rajak@consultant.volvo.com>
2021-08-24 11:17:46 +0000

    test

--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -3,12 +3,18 @@ sap.ui.define([
     "sap/m/MessageBox",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/core/Fragment"
+    "sap/ui/core/Fragment",
+    "sap/ui/core/ValueState",
+    "sap/m/Dialog",
+    "sap/m/DialogType",
+    "sap/m/Button",
+    "sap/m/ButtonType",
+    "sap/m/Text"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-    function (Controller, MessageBox, MessageToast, JSONModel, Fragment) {
+    function (Controller, MessageBox, MessageToast, JSONModel, Fragment, ValueState, Dialog, DialogType, Button, ButtonType, Text) {
         "use strict";
 
         return Controller.extend("pro.proj.controller.View", {
@@ -21,11 +27,31 @@ sap.ui.define([
 
             },
             handleQuickViewPress: function (oEvent) {
-                MessageBox.information("Execute Order Yahan contact aayega");
+
+                if (!this.oDefaultMessageDialog) {
+                    this.oDefaultMessageDialog = new Dialog({
+                        type: DialogType.Message,
+                        title: "Contact Details",
+                        content: new Text({ text: "Hello There , This is Manish" }),
+                       
+                        beginButton: new Button({
+                            type: ButtonType.Emphasized,
+                            text: "OK",
+                            press: function () {
+                                this.oDefaultMessageDialog.close();
+                            }.bind(this)
+                        }),
+                        endButton: new Button({
+                            text: "Cancel",
+                            press: function () {
+                                this.oDefaultMessageDialog.close();
+                            }.bind(this)
+                        })
+                    });
+                }
+                this.oDefaultMessageDialog.open();
 
             }
-
-
 
         });
     });
