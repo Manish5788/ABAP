84dc5308076abcf5a6047401c081e118daef4edd
MANISH RAJAK <manish.kumar.rajak@consultant.volvo.com>
2021-08-24 08:11:10 +0000

    test

--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -13,36 +13,18 @@ sap.ui.define([
 
         return Controller.extend("pro.proj.controller.View", {
             onInit: function () {
-                var oEmployeeModel = new JSONModel(sap.ui.require.toUrl("model/EmployeeData.json"));
-
-                this.getView().setModel(oEmployeeModel, "EmployeeModel")
-            },
-
-            onAfterRendering: function () {
-
-                var oButton = this.byId("employeeQuickView");
-                oButton.$().attr("aria-haspopup", true);
 
             },
+
             handlePopoverPress: function (oEvent) {
                 MessageBox.information("Execute Order 66");
 
             },
-            handleEmployeeQuickViewPress: function (oEvent) {
-                var oModel = this.getView().getModel("EmployeeModel");
-                 MessageToast.show("contact button was clicked");
-            },
-            onNavigate: function (oEvent) {
-                var oNavOrigin = oEvent.getParameter("navOrigin");
-                if (oNavOrigin) {
-                    MessageToast.show("Link '" + oNavOrigin.getText() + "' was clicked");
-                } else {
-                    MessageToast.show("Back button was clicked");
-                }
-            },
-            onAvatarPressed: function () {
-                MessageToast.show("Avatar pressed!");
+            handleQuickViewPress: function (oEvent) {
+                MessageBox.information("Execute Order Yahan contact aayega");
+
             }
+
 
 
         });
